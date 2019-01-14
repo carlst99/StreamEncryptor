6bcb23a8ed3d764fddb1eff6ea6e39a28fea99f2
Carl Stephens <carlst99@outlook.co.nz>
2019-01-14 14:38:30 +1300

    Add more tests for authentication

--- StreamEncryptor.Tests/AesHmacEncryptorTests.cs
+++ StreamEncryptor.Tests/AesHmacEncryptorTests.cs
@@ -18,6 +18,7 @@ namespace StreamEncryptor.Tests
             {
                 MemoryStream encrypted = await encryptor.EncryptAsync<MemoryStream>(ms).ConfigureAwait(false);
                 Assert.True(await encryptor.AuthenticateAsync(encrypted).ConfigureAwait(false));
+                Assert.True(encrypted.Position == 0);
             }
         }
 
@@ -40,6 +41,43 @@ namespace StreamEncryptor.Tests
 
                 MemoryStream tampered = new MemoryStream(buffer);
                 Assert.False(await encryptor.AuthenticateAsync(tampered).ConfigureAwait(false));
+                Assert.True(tampered.Position == 0);
+            }
+        }
+
+        [Fact]
+        public async void TestAuthenticateNoPeekTrue()
+        {
+            MemoryStream ms = new MemoryStream(RANDOM_BYTES);
+
+            using (AesHmacEncryptor encryptor = new AesHmacEncryptor(PASSWORD))
+            {
+                MemoryStream encrypted = await encryptor.EncryptAsync<MemoryStream>(ms).ConfigureAwait(false);
+                Assert.True(await encryptor.AuthenticateAsync(encrypted, false).ConfigureAwait(false));
+                Assert.True(encrypted.Position != 0);
+            }
+        }
+
+        [Fact]
+        public async void TestAuthenticateNoPeekFalse()
+        {
+            MemoryStream ms = new MemoryStream(RANDOM_BYTES);
+
+            using (AesHmacEncryptor encryptor = new AesHmacEncryptor(PASSWORD))
+            {
+                MemoryStream encrypted = await encryptor.EncryptAsync<MemoryStream>(ms).ConfigureAwait(false);
+                byte[] buffer = encrypted.GetBuffer();
+
+                byte substitute = 0;
+                do
+                {
+                    buffer[0] = substitute++;
+                    substitute = substitute++;
+                } while (buffer[0] == substitute);
+
+                MemoryStream tampered = new MemoryStream(buffer);
+                Assert.False(await encryptor.AuthenticateAsync(tampered, false).ConfigureAwait(false));
+                Assert.True(tampered.Position != 0);
             }
         }
     }
